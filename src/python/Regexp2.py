c06f0bde02a7ae75991b747c689ed4e9dc350fbb
Mathias Nyman <mcnyman@cc.hut.fi>
2012-03-08 21:11:03 +0200

    src-python: Added initial python regexp test.

---
+++ src/python/Regexp2.py
@@ -0,0 +1,26 @@
+import sys
+import re
+import os
+
+def main():
+    f = open(sys.argv[1])
+    lines = f.readlines()
+    f.close()
+
+    #Pre-compile regexp before comparing to it
+    #TODO: this should be excluded from "empty"-run
+    if len(sys.argv) > 2 and sys.argv[2] == '--with-or-operator':
+        pattern = re.compile(r"\d+|\w+\d+\.")
+    else:
+        pattern = re.compile(r"\d+")
+
+    amountFound = 0
+    for line in lines:
+        match = pattern.search(line)
+        if match is not None:
+            amountFound = amountFound + 1
+
+if __name__ == '__main__':
+    main()
+    print os.times(), 'TODO: which of these do we measure?'
+
