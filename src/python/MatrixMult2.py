396a244ec551c03f6bb117ece2150d186aa89b0b
Mathias Nyman <mcnyman@cc.hut.fi>
2012-03-08 22:03:20 +0200

    src-python: Added handling of float or int in MatrixMult.

--- src/python/MatrixMult2.py
+++ src/python/MatrixMult2.py
@@ -5,15 +5,18 @@ def main():
     f = open(sys.argv[1])
     lines = f.readlines()
     f.close()
-    result = 0
     dimension = int(sys.argv[2])
+    itemType = 1 if len(sys.argv) > 3 and sys.argv[3] == '--float' else 0
     matrix = []
     #TODO: this might not be the proper solution
     #      if input file is not in "matrix" format also
     for idx, line in enumerate(lines):
         if idx == dimension:
             break
-        matrix[idx] = [int(i) for i in line.split()][:dimension]
+        if itemType == 0:
+            matrix[idx] = [int(i) for i in line.split()][:dimension]
+        else:
+            matrix[idx] = [float(i) for i in line.split()][:dimension]
 
     #TODO: how did matrix multiplication go again...?
     result = []
