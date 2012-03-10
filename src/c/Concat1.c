2078c1aafd42d15e4f582f1b5703956a31dc1b86
Jukka Aro <jukka.aro@aalto.fi>
2012-03-10 21:10:42 +0200

    src-c: initial implementation for Concat1C.c

--- src/c/Concat1.c
+++ src/c/Concat1.c
@@ -1,3 +1,56 @@
-int main() {
+/*
+ *  MatrixMult1.c
+ *  
+ *
+ *  Created by Jukka Aro on 10.3.2012.
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define MAX_LINE_LENGTH 3000
+#define MAX_WORD_LENGTH 80
+
+void concatenate(const char *input_file)
+{
+	FILE *f = fopen(input_file, "r");
+	
+	if(f == 0)
+	{
+		fprintf(stderr, "Could not open file %s\n", input_file);
+		exit(1);
+	}
+	
+	char result[MAX_LINE_LENGTH];
+	char tmp[MAX_WORD_LENGTH];
+
+	clock_t total = 0;
+
+	while(EOF != fscanf(f, "%s", tmp))
+	{
+		clock_t start = clock();
+		strcat(result, tmp);
+		clock_t end = clock();
+		total += end-start;
+	}
+
+	printf("%lf\n", ((double)total)/CLOCKS_PER_SEC);
+
+	fclose(f);
+}
+
+int main(int argc, char **argv)
+{
+	if(argc != 2)
+	{
+		fprintf(stderr, "Illegal number of arguments\n");
+		exit(1);
+	}
+	
+	char *input_file = argv[1];
+	concatenate(input_file);
+	
     return 0;
 }
