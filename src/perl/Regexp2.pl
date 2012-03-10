c4f879dcc8335e15a434ad875ad177cb14ead5a4
Mathias Nyman <mcnyman@cc.hut.fi>
2012-03-10 17:44:17 +0200

    src-perl: Finally really fixed pre compiled regexp match counting.

--- src/perl/Regexp2.pl
+++ src/perl/Regexp2.pl
@@ -8,14 +8,13 @@ sub main {
     close FH;
     my $withOrOperator = ($#ARGV > 0 and $ARGV[1] eq '--with-or-operator') ? 1:0;
     my $amountFound = 0;
-    my $pattern = qr/\d+|\w+\d+\./;
+    my $pattern = qr/\d+/;
     if ($withOrOperator) {
         $pattern = qr/\d+|\w+\d+\./;
     }
     foreach (@lines) {
-        $amountFound += /$pattern/;
+        $amountFound++ while (/$pattern/g);
     }
-    print $amountFound, "\n";
 }
 
 my $start = Time::HiRes::gettimeofday();
