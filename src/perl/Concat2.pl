b1ae20150983b54b68948db0b4ad514657fcb155
Mathias Nyman <mcnyman@cc.hut.fi>
2012-03-10 11:45:21 +0200

    src-perl: Added initial Concat test.

---
+++ src/perl/Concat2.pl
@@ -0,0 +1,17 @@
+use strict;
+use warnings;
+use Time::HiRes;
+
+sub main {
+    open FH, $ARGV[0];
+    my @lines = <FH>;
+    close FH;
+    my $concatenated = join '', @lines;
+}
+
+my $start = Time::HiRes::gettimeofday();
+main();
+my $end = Time::HiRes::gettimeofday();
+my ($user, $system, $child_user, $child_system) = times;
+print $user,' ',$system,' ',$child_user,' ',$child_system,' ',($end - $start)
+
