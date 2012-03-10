0a9cb3ae2d3aedf3acfe487e2567987d1ad73456
Mathias Nyman <mcnyman@cc.hut.fi>
2012-03-10 19:24:54 +0200

    src-java: Added initial Regexp2, missing cpu measurement.

--- src/java/Regexp2.java
+++ src/java/Regexp2.java
@@ -1,4 +1,34 @@
+import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.regex.*;
+
 class Regexp2 {
     public static void main(String[] args) {
+        List<String> lines = new ArrayList<String>();
+        try {
+          BufferedReader br= new BufferedReader(new FileReader(args[0]));
+          String str;
+          while ((str = br.readLine()) != null) {
+              lines.add(str + '\n');
+          }
+          br.close();
+        } catch (Exception e) { System.out.println(e); }
+
+        Pattern pattern = Pattern.compile("\\d+");
+        if (args.length > 1 && args[1].equals("--with-or-operator")) {
+            pattern = Pattern.compile("\\d+|\\w+\\d+\\.");
+        }
+
+        Matcher match;
+        int amountFound = 0;
+        for (int i=0; i < lines.size(); i++) {
+            match = pattern.matcher(lines.get(i));
+            while (match.find()) {
+                amountFound++;
+            }
+        }
+
+        //TODO: add cpu time measurement
     }
 }
