d6a3774f43fc9ad4f4ee9459fca4b460c40c479c
Mathias Nyman <mcnyman@cc.hut.fi>
2012-03-11 11:01:59 +0200

    src-python: Fixed MatrixMult2 to compile and work.

--- src/python/MatrixMult2.py
+++ src/python/MatrixMult2.py
@@ -7,23 +7,20 @@ def main():
     f.close()
     dimension = int(sys.argv[2])
     itemType = 1 if len(sys.argv) > 3 and sys.argv[3] == '--float' else 0
-    matrix = []
-    #TODO: this might not be the proper solution
-    #      if input file is not in "matrix" format also
+    matrix = [0]*dimension
+
     if itemType == 0:
         for idx, line in enumerate(lines):
             if idx == dimension:
                 break
-                matrix[idx] = [int(i) for i in line.split()][:dimension]
+            matrix[idx] = [int(float(i)) for i in line.split()][:dimension]
     else:
         for idx, line in enumerate(lines):
             if idx == dimension:
                 break
             matrix[idx] = [float(i) for i in line.split()][:dimension]
 
-    #TODO: how did matrix multiplication go again...?
-    result = []
-    dimension = len(matrix)
+    result = [[0]*dimension]*dimension
     for i in range(dimension):
         for j in range(dimension):
             for k in range(dimension):
