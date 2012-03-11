8387bbe024f5ea2fe937e216e525b556cc560711
Mathias Nyman <mcnyman@cc.hut.fi>
2012-03-11 21:55:30 +0200

    src-cpp: Added Concat2, without time measurement.

--- src/cpp/Concat2.cpp
+++ src/cpp/Concat2.cpp
@@ -1,3 +1,19 @@
-int main() {
+#include <string>
+#include <istream>
+#include <fstream>
+
+using namespace std;
+int main(int argv, char** argc) {
+    ifstream inStream;
+    inStream.open(argc[1]);
+
+    string concatenated;
+    string tmp;
+    while (inStream.good()) {
+        getline(inStream, tmp);
+        concatenated += tmp + '\n';
+    }
+    inStream.close();
+
     return 0;
 }
