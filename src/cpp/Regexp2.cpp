953dc1e18914b7aa49a988c768fff3da745719d8
Mathias Nyman <mcnyman@cc.hut.fi>
2012-03-12 21:10:39 +0200

    src-cpp: Initial skeleton for Regexp2.

--- src/cpp/Regexp2.cpp
+++ src/cpp/Regexp2.cpp
@@ -1,3 +1,35 @@
-int main() {
+#include <string>
+#include <istream>
+#include <fstream>
+#include <vector>
+#include <regex>
+
+using namespace std;
+int main(int argv, char** argc) {
+    ifstream inStream;
+    inStream.open(argc[1]);
+
+    //NOTE: C++0x standard's regex has not been done yet
+    //      See url for up-to-date information:
+    //      http://gcc.gnu.org/onlinedocs/libstdc++/manual/status.html#status.iso.200x
+    //regex pattern("\\d+");
+    if (argv > 2 and argc[2] == "--with-or-operator") {
+        //pattern = regex("\\d+|\\w+\\d+\\.");
+    }
+
+    vector<string> lines;
+    string line;
+    while (inStream.good()) {
+        getline(inStream, line);
+        lines.push_back(line + '\n');
+    }
+    inStream.close();
+
+    vector<string>::const_iterator it = lines.begin();
+    vector<string>::const_iterator end = lines.end();
+    for (; it != end; ++it) {
+    }
+
+
     return 0;
 }
