792389ad02c94210ad47d569d9dd243c91755fc0
Jukka Aro <jukka.aro@aalto.fi>
2012-03-14 20:54:35 +0200

    src-perl: Implementation of Concat1.pl without --dry-run

---
+++ src/perl/Concat1.pl
@@ -0,0 +1,23 @@
+#
+# Concat1.pl
+#
+# Created by Jukka Aro on 14.3.2012.
+#
+
+sub concatenate{
+	open FILE, $_[0] or die $!;
+	$res = "";
+	while(<FILE>){
+		chomp();
+		$res .= $_;
+	}
+	close(FILE);
+}
+
+#program entry:
+
+if($#ARGV != 0){
+	die "Illegal number of arguments";
+}
+
+&concatenate($ARGV[0]);
