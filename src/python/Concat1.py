b3601a9e250541c0436a078445240c67cfeee11d
Jukka Aro <jukka.aro@aalto.fi>
2012-03-17 16:09:03 +0200

    src-python: added '--dry-run' option to *1.py

--- src/python/Concat1.py
+++ src/python/Concat1.py
@@ -6,17 +6,26 @@
 
 import sys
 
-def concatenate(input_file):
+def concatenate(input_file, dry_run):
 	f = open(input_file, 'r')
-	s = '';
-	for line in f:
-		s += line.rstrip()
+	if dry_run:
+		for line in f:
+			line.rstrip()
+	else:
+		s = '';
+		for line in f:
+			s += line.rstrip()
 	f.close()
 
 
 # program entry:
 
-if len(sys.argv) != 2:
+if len(sys.argv) < 2 or len(sys.argv) > 3:
 	print "Illegal number of arguments"
 	sys.exit()
-concatenate(sys.argv[1])
+if len(sys.argv)  == 3 and sys.argv[2] == '--dry-run':
+	dry_run = True
+else:
+	dry_run = False
+	
+concatenate(sys.argv[1], dry_run)
