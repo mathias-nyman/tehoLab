823dbef6f7d0cd477f82ca44e3d3fc32aac921f9
Jukka Aro <jukka.aro@aalto.fi>
2012-04-02 19:10:36 +0300

    src-pl: fixed dry-run in *2.pl; src-c: fixed max line length in Concat1.c; data: longer data file for concat

--- src/perl/Concat2.pl
+++ src/perl/Concat2.pl
@@ -5,7 +5,7 @@ sub main {
     open FH, $ARGV[0];
     my @lines = <FH>;
     close FH;
-    if ($#ARGV > 1 and $ARGV[1]) {
+    if ($#ARGV > 0 and $ARGV[1]) {
         return;
     }
     my $concatenated = join '', @lines;
